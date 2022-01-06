0fcf016f3de8800204eeb6b4ef6379462b4d11f1
Diego Pedroso <diego.pedroso@dott.pt>
2022-01-06 12:19:17 -0300

    ansible playbook to deploy k8s on premise

---
+++ infrastructure/kube-cluster/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -xe
+
+ansible-playbook -i hosts initial.yml
+ansible-playbook -i hosts kube-dependencies.yml
+ansible-playbook -i hosts master.yml
+ansible-playbook -i hosts workers.yml
