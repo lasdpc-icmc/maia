8654bd1b139e349dc245c90bd2b4cd29f8a9b18b
Diego Pedroso <diego.pedroso@dott.pt>
2022-02-19 15:24:54 -0300

    test CircleCI workflow

--- .circleci/rollback.sh
+++ .circleci/rollback.sh
@@ -2,17 +2,11 @@
 set -xe
 
 source ./.circleci/common.sh;
-env_vars
-k8s_dependencies
 
 # Install dependencies
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates gnupg jq kubectl google-cloud-sdk
+k8s_dependencies
+env_vars
 
 # Set current cluster
 
