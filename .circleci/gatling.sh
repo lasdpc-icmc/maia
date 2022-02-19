52fd1d78484072adabdaac34ccef0cef7ad35598
Diego Pedroso <diego.pedroso@dott.pt>
2022-02-19 15:39:52 -0300

    test circleCI workflow

--- .circleci/gatling.sh
+++ .circleci/gatling.sh
@@ -12,7 +12,7 @@ export GATLING_HOME="/home/circleci/project/gatling-charts-highcharts-bundle-2.3
 wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/2.3.1/gatling-charts-highcharts-bundle-2.3.1-bundle.zip
 unzip gatling-charts-highcharts-bundle-2.3.1-bundle.zip
 rm $GATLING_HOME/user-files/simulations/computerdatabase/*.scala
-cp /home/circleci/project/$APP/loadtest/* $GATLING_HOME/user-files/simulations/computerdatabase/
+cp /home/circleci/project/apps/$APP/loadtest/* $GATLING_HOME/user-files/simulations/computerdatabase/
 $GATLING_HOME/bin/gatling.sh -s computerdatabase.BasicSimulation
 
 cd $GATLING_HOME/results/
