287bd59f3ce8b36c65c2f20b43d908be768dc5e1
Diego Pedroso <diego.pedroso@dott.pt>
2022-02-19 15:17:36 -0300

    test CircleCI workflow

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -xe
 source ./.circleci/common.sh;
-env_vars
 
 # Load common functions and Install dependencies
 k8s_dependencies
+env_vars
 
 # Setup Cluster
 
 echo $TOKEN_CLUSTER | base64 --decode --ignore-garbage > /tmp/config
 
-
 # Deploy
 
 KUBE_CONFIG=$(cat apps/$APP/kubernetes/values.yaml | sed "s|CIRCLE_TAG_REPLACE|$TAG|g")
-echo "$KUBE_CONFIG" | kubectl --kubeconfig=/tmp/config apply -f -
-kubectl rollout status deployment/$APP -n $APP
+echo "$KUBE_CONFIG" | kubectl_run apply -f -
+kubectl_run rollout status deployment/$APP -n $APP
 get_pod
