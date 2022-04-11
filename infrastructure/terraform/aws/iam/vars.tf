dee89c6115a8fa37c22eca7706884001fbb644c8
Diego Pedroso <diego.pedroso@dott.pt>
2022-04-11 01:00:32 +0100

    setup IAM, users and groups using terraform

---
+++ infrastructure/terraform/aws/iam/vars.tf
@@ -0,0 +1,21 @@
+variable "username" {
+  type = list(string)
+}
+variable "username_dev" {
+  type = list(string)
+}
+variable "username_admins" {
+  type = list(string)
+}
+variable "username_read_only" {
+  type = list(string)
+}
+variable "env" {
+  type = string
+}
+variable "resource_name" {
+  type = string
+}
+variable "region" {
+  type = string
+}
