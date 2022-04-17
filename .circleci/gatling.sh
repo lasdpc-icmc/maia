77e8d252f17533f4dea00e486e00d7c6be9eb8c3
Diego Pedroso <diego.pedroso@dott.pt>
2022-04-17 20:00:51 +0100

    change results tests path

--- .circleci/gatling.sh
+++ .circleci/gatling.sh
@@ -4,6 +4,7 @@ source ./.circleci/common.sh;
 env_vars
 
 # Install Dependencies
+
 export GATLING_HOME="/home/circleci/project/gatling-charts-highcharts-bundle-2.3.1"
 install_awscli_kubectl
 aws_credentials
@@ -14,11 +15,12 @@ wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcha
 unzip gatling-charts-highcharts-bundle-2.3.1-bundle.zip
 rm $GATLING_HOME/user-files/simulations/computerdatabase/*.scala
 cp /home/circleci/project/apps/$APP/loadtest/* $GATLING_HOME/user-files/simulations/computerdatabase/
-$GATLING_HOME/bin/gatling.sh -s computerdatabase.BasicSimulation
 
-cd $GATLING_HOME/results/
-tar -cvf $APP-$(date +%Y-%m-%d).tar.gz *
+# Run Simulation
+
+$GATLING_HOME/bin/gatling.sh -s computerdatabase.BasicSimulation
+cd $GATLING_HOME/results/ && tar -cvf $APP-$(date +%Y-%m-%d)-$CIRCLE_PREVIOUS_BUILD_NUM.tar.gz *
 
 # Push results
 
-aws_run s3 sync $GATLING_HOME/results/$APP-$(date +%Y-%m-%d).tar.gz s3://gatling-metrics-application-results/$APP-$(date +%Y-%m-%d).tar.gz
+aws_run s3 cp $GATLING_HOME/results/$APP-$(date +%Y-%m-%d)-$CIRCLE_PREVIOUS_BUILD_NUM.tar.gz * s3://gatling-metrics-application-results/$APP
