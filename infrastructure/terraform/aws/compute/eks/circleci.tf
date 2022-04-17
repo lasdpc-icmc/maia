897d7025ed3e659ec6b276c0470e3ba340870c80
Diego Pedroso <diego.pedroso@dott.pt>
2022-04-17 17:38:38 +0100

    create circleci useraccount to deploy applications

---
+++ infrastructure/terraform/aws/compute/eks/circleci.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_user" "usp-eks-deploy" {
+  name = "${terraform.workspace}-eks-circleci"
+}
+
+resource "aws_iam_access_key" "usp-eks-deploy" {
+  user = aws_iam_user.usp-eks-deploy.name
+}
+
+data "aws_iam_policy_document" "usp-eks-deploy" {
+  statement {
+    sid       = "1"
+    actions   = ["eks:*"]
+    resources = [module.aws_eks.cluster_arn]
+  }
+}
+
+resource "aws_iam_user_policy" "usp-eks-deploy" {
+  name   = "${terraform.workspace}-circleci-eks"
+  user   = aws_iam_user.usp-eks-deploy.name
+  policy = data.aws_iam_policy_document.usp-eks-deploy.json
+}
+
+output "secret" {
+  value = aws_iam_access_key.usp-eks-deploy.secret
+  sensitive = true
+}
