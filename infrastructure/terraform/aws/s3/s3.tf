51ae891b9619136f8d880a4c9865583595c842be
Diego Pedroso <diego.pedroso@dott.pt>
2022-04-17 18:51:24 +0100

    define automated loadtest with gatling and upload results on S3

--- infrastructure/terraform/aws/s3/s3.tf
+++ infrastructure/terraform/aws/s3/s3.tf
@@ -2,3 +2,8 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = var.app_name
   tags = local.common_tags
 }
+
+resource "aws_s3_bucket" "gatling_results" {
+  bucket = var.app_gatling
+  tags = local.common_tags
+}
