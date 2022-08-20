9ab21d3af07e6b946c0e7072e3474dc12092a361
sam <sam@dean.winchester>
2022-08-19 22:03:04 -0300

    fix: spot price

--- infrastructure/terraform/aws/compute/eks/eks.tf
+++ infrastructure/terraform/aws/compute/eks/eks.tf
@@ -41,7 +41,7 @@ module "aws_eks" {
     asg_max_size          = "2"
     asg_min_size          = "1"
     instance_type         = "r6a.8xlarge"
-    spot_price            = var.spot_price
+    spot_price            = "0.50"
     root_volume_size      = "120"
     root_volume_type      = var.root_volume_type
     key_name              = var.key_pair
