4ff4769f2dcba7ee74ebde8ae305ebb831ceca14
Diego Pedroso <diego.pedroso@dott.pt>
2022-09-11 15:43:01 +0100

    deploy EKS cilium cluster

---
+++ infrastructure/terraform/aws/compute/eks-cilium/vars.tf
@@ -0,0 +1,60 @@
+variable "env" {
+  type = string
+}
+variable "key_pair" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "resource_name" {
+  type = string
+}
+variable "vpc_name" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
+variable "max_size" {
+  type = string
+}
+variable "min_size" {
+  type = string
+}
+variable "min_size_on_demand" {
+  type = string
+}
+variable "max_size_on_demand" {
+  type = string
+}
+variable "desired_capacity" {
+  type = string
+}
+variable "desired_capacity_on_demand" {
+  type = string
+}
+variable "spot_price" {
+  type = string
+}
+variable "root_volume_size" {
+  type = string
+}
+variable "root_volume_type" {
+  type = string
+}
+variable "cluster_version" {
+  type = string
+}
+variable "key" {
+  type = string
+}
+variable "organization" {
+  type = string
+}
+variable "workspace" {
+  type = string
+}
+variable "users" {
+  type = list(string)
+}
