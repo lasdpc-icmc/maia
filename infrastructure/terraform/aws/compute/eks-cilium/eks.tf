c8b816ae59f3e7d53cb242fc39424296c3a9a3cd
Diego Pedroso <diego.pedroso@dott.pt>
2022-09-11 17:14:55 +0100

    deploy cilium EKS

--- infrastructure/terraform/aws/compute/eks-cilium/eks.tf
+++ infrastructure/terraform/aws/compute/eks-cilium/eks.tf
@@ -27,7 +27,7 @@ locals {
 
 module "aws_eks" {
   source          = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v16.0.0"
-  cluster_name    = "${var.workspace}-${var.env}-${var.resource_name}"
+  cluster_name    = "${terraform.workspace}-${var.env}"
   subnets         = data.terraform_remote_state.vpc.outputs.pub_sn_id
   vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
   cluster_version = var.cluster_version
@@ -40,7 +40,7 @@ module "aws_eks" {
     spot_price            = var.spot_price
     root_volume_size      = var.root_volume_size
     root_volume_type      = var.root_volume_type
-    key_name              = "${var.workspace}-${var.env}"
+    key_name              = "${terraform.workspace}-${var.env}"
     ebs_optimized         = true
     public_ip             = false
     autoscaling_enabled   = true
@@ -55,7 +55,7 @@ module "aws_eks" {
     spot_price            = "0.65"
     root_volume_size      = "500"
     root_volume_type      = var.root_volume_type
-    key_name              = "${var.workspace}-${var.env}"
+    key_name              = "${terraform.workspace}-${var.env}"
     ebs_optimized         = true
     public_ip             = false
     autoscaling_enabled   = true
@@ -66,14 +66,14 @@ module "aws_eks" {
   tags = local.common_tags
    map_users = concat([
     {
-      userarn  = "arn:aws:iam::${data.aws_caller_identity.this.account_id}:user/${var.workspace}-aws-eks-circleci"
-      username = "${var.workspace}-aws-eks-circleci"
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.this.account_id}:user/${terraform.workspace}-aws-eks-circleci"
+      username = "${terraform.workspace}-aws-eks-circleci"
       groups   = ["system:masters"]
     }    
   ], local.auth_users)
 }
 
 resource "aws_key_pair" "eks-cilium" {
-  key_name   = "${var.workspace}-${var.env}"
+  key_name   = "${terraform.workspace}-${var.env}"
   public_key = var.key
 }
