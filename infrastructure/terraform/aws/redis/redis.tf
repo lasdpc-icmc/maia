f545ca7c4ec05f705056f16a2eda8945f790d080
Diego Frazatto Pedroso <diego.pedroso@vodafone.com>
2023-04-22 15:32:49 +0100

    set subnet id on redis instance

--- infrastructure/terraform/aws/redis/redis.tf
+++ infrastructure/terraform/aws/redis/redis.tf
@@ -9,7 +9,12 @@ resource "aws_elasticache_cluster" "lasdpc-icmc" {
   tags                 = local.common_tags
   subnet_group_name    = aws_elasticache_subnet_group.lasdpc-vpc.name
   security_group_ids  = [aws_security_group.redis_sg.id]
-}                                                                  
+}
+
+resource "aws_elasticache_subnet_group" "lasdpc-vpc" {
+  name       = "${var.namespace}-cache-subnet"
+  subnet_ids = ["subnet-05fdd4b7eaea01c20","subnet-07d9d78240501ee27", "subnet-0de52e8cce5e02bed", "subnet-00f15c24c17c442d9"]
+}                                                                           
 
 resource "aws_security_group" "redis_sg" {
   name        = "${var.app_name}-${var.env}-sg"
