4e83651f6a2eda293d27ba9f96e24d137d4f2f8a
Diego Frazatto Pedroso <diego.pedroso@vodafone.com>
2023-04-22 15:42:36 +0100

    create a redis replicaset

--- infrastructure/terraform/aws/redis/config.tf
+++ infrastructure/terraform/aws/redis/config.tf
@@ -6,17 +6,15 @@ locals {
     "project"     = "platform"
   }
 }
-
 provider "aws" {
   region  = var.region
   version = "3.10.0"
 }
-
 terraform {
-  required_version = ">= 0.12.24"
-  backend "s3" {
-    bucket  = "lasdpc-terraform-states"
-    key     = "aws/s3/terraform.tfstate"
-    region  = "us-east-1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.64.0"
+    }
   }
 }
