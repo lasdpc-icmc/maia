60fbd38b35fd0b49bdce568c3bd0cb46c1441c88
Diego Frazatto Pedroso <diego.pedroso@vodafone.com>
2023-04-22 16:54:19 +0100

    create a redis replicaset

--- infrastructure/terraform/aws/redis/redis.tf
+++ infrastructure/terraform/aws/redis/redis.tf
@@ -9,13 +9,17 @@ resource "aws_elasticache_replication_group" "lasdpc-icmc" {
   replication_group_id        = "${var.app_name}-${var.env}"
   description                 = "Redis to store assets from DeepLog Training"
   node_type                   = var.node_type
+  num_cache_clusters          = var.num_cache_nodes
   parameter_group_name        = var.parameter_group_name
   port                        = var.redis_port
   security_group_ids          = [aws_security_group.redis_sg.id]
   tags                        = local.common_tags
   subnet_group_name           = aws_elasticache_subnet_group.lasdpc-vpc.name
-  num_node_groups         = 2
-  replicas_per_node_group = 1
+
+
+  lifecycle {
+    ignore_changes = [num_cache_clusters]
+  }
 }
 
 resource "aws_elasticache_cluster" "replica" {
