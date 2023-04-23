771955812e37c0fd460239d208302b320f9e15f4
Diego Frazatto Pedroso <diego.pedroso@vodafone.com>
2023-04-23 13:51:51 +0100

    add s3 bucket to app_thridyparty trainning

---
+++ infrastructure/terraform/aws/s3/vars.tf
@@ -0,0 +1,24 @@
+variable "app_name" {
+  type = string
+}
+variable "app_gatling" {
+  type = string
+}
+variable "app_locust" {
+  type = string
+}
+variable "app_deeplog" {
+  type = string
+}
+variable "app_thirdparty" {
+  type = string
+}
+variable "resource_name" {
+  type = string
+}
+variable "env" {
+  type = string
+}
+variable "region" {
+  type = string
+}
