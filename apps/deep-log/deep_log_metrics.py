4f6dffa943a956a27717e5fc5a96f11bd49e76ad
Pedro Vinicius <61328379+derSchmetterling@users.noreply.github.com>
2023-05-28 15:07:16 -0300

    adds train and predict to deeplog
    adds code to train and save model, load model and upload results of prediction to s3

---
+++ apps/deep-log/deep_log_metrics.py
@@ -0,0 +1,105 @@
+import numpy as np
+from sklearn.metrics import classification_report
+import torch
+
+
+def save_model(deeplog, name):
+    '''
+    Saves deeplog state dictionary
+    '''
+
+    torch.save(deeplog.state_dict(), f'deeplog_{name}.pth')
+
+
+def load_model(deeplog,path_to_pth):
+
+    '''
+    Reloads deeplog model
+    '''
+
+    deeplog.load_state_dict(torch.load(path_to_pth))
+
+
+
+
+
+
+
+## Considerando apenas o primeiro elemento do vetor de predição, o modelo acertou o próximo evento?
+def get_ind_metrics(y_test, y_pred):
+    '''
+    Return metrics by template of logs
+
+    y_test: Tensor, generally output of deeplog.preprocessor 
+    y_pred: Tensor, predictions of deep log, output of deeplog.predict
+    
+    '''
+    individual_pred = classification_report(
+                                y_true = y_test.cpu().numpy(),
+                                y_pred = y_pred[:, 0].cpu().numpy(),
+                                digits = 4,
+                                zero_division = 0,
+                                output_dict= True
+                                                )
+    
+
+    return individual_pred
+
+
+
+
+# O próximo evento está em algumas dos elementos do vetor de predição?
+# Se sim, o evento é classificado como normal, se não, é classificado como anomalia
+def is_anomaly(y_test,y_pred):
+
+    '''
+    Returns a boolean Tensor, the elements assumes True in case of anomalies and False otherwise
+
+
+    y_test: Tensor, generally output of deeplog.preprocessor 
+    y_pred_normal: Tensor, predictions of deep log, output of deeplog.predict
+
+    '''
+
+    anomalies_normal = ~torch.any(
+        y_test == y_pred.T,
+        dim = 0,
+    )
+    
+    return anomalies_normal
+
+
+
+#Compute classification report for anomalies
+
+# Se soubermos que um conjunto de logs contém anomalias e outro não
+def overall_metrics(anomalies_normal, anomalies_abnormal):
+    '''
+    If we have a batch of logs that we know beforehand that are not anomalous
+    and a batch of logs that we know are anomalous, we can compare the performance of deeplog in both classes
+
+
+    anomalies_normal: boolean Tensor, containing classifications of deeplog for normal logs (output of is_anomaly function)
+    anomalies_abnormal: boolean Tensor, containing classifications of deeplog for abnormal logs (output of is_anomaly function)
+
+    '''
+
+
+
+    # Compute classification report for anomalies
+    y_pred = torch.cat((anomalies_normal, anomalies_abnormal))
+    y_true = torch.cat((
+        torch.zeros(anomalies_normal  .shape[0], dtype=bool),
+        torch.ones (anomalies_abnormal.shape[0], dtype=bool),
+    ))
+
+    res = classification_report(
+        y_pred       = y_pred.cpu().numpy(),
+        y_true       = y_true.cpu().numpy(),
+        labels       = [False, True],
+        target_names = ["Normal", "Anomaly"],
+        digits       = 4,
+        output_dict=True
+    )
+
+    return res
---
+++ apps/deep-log/deep_log_train.py
@@ -0,0 +1,76 @@
+
+import torch
+import aws_tools
+import os
+from drain_parser import file_name
+
+
+
+# Import DeepLog and Preprocessor
+from deeplog import DeepLog
+from deeplog.preprocessor import Preprocessor
+from sklearn.metrics import classification_report
+
+
+# Imports for showing metrics
+import numpy as np
+import json
+
+
+
+from data_cleaning import clean_sock, read_logs, write_logs
+from deeplog_metrics import get_ind_metrics, is_anomaly, save_model, load_model
+
+
+
+
+
+
+##############################################################################
+#                                 Load data                                  #
+##############################################################################
+
+
+
+# Create preprocessor for loading data
+#length - tamanho da janela a ser considerada
+preprocessor = Preprocessor(
+    length  = 10,           # Extract sequences of 20 items
+    timeout = float('inf'), # Do not include a maximum allowed time between events
+)
+
+# Dowload the file from S3
+prefix = "clean/"
+#aws_tools.get_to_s3(f'cluster_{file_name}', prefix)
+
+# Load normal data from s3
+X, y, label, mapping = preprocessor.text(
+    path    = f'cluster_{file_name}',
+    verbose = True,
+    # nrows   = 10_000, # Uncomment/change this line to only load a limited number of rows
+)
+
+
+
+##############################################################################
+#                                 Train deeplog                              #
+##############################################################################
+# Create DeepLog object
+#output_size - número de chaves diferentes, geralmente output_size = length
+deeplog = DeepLog(
+    input_size  = 30, # Number of different events to expect
+    hidden_size = 64 , # Hidden dimension, we suggest 64
+    output_size = 30, # Number of different events to expect
+)
+
+# Train deeplog
+deeplog.fit(
+    X          = X,
+    y          = y,
+    epochs     = 30 ,
+    batch_size = 128,
+    optimizer  = torch.optim.Adam,
+)
+
+
+save_model(deeplog, 'deeplog_model_v1.pth')
