8f568bbce1fc52f2c1cafacdc1f32badf29e13f4
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2023-06-02 09:24:02 -0300

    adding more getObject permission for locust-distributor role

--- infrastructure/terraform/aws/iam/locust_metrics_distributor.tf
+++ infrastructure/terraform/aws/iam/locust_metrics_distributor.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "locust-distributor-policydocument" {
     effect    = "Allow"
     actions   = [
       "s3:ListBucket",
-      "s3:GetObject"
+      "s3:GetObject*"
     ]
     resources = [
       "arn:aws:s3:::${var.bucket_locust_name}",
