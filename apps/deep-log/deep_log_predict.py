13024c6e5c19a0bfd9b9f4e1878c298612baf0b2
Pedro Alves <alvespedro769@usp.br>
2023-06-08 12:26:58 -0300

    modularizes code to train and predict

--- apps/deep-log/deep_log_predict.py
+++ apps/deep-log/deep_log_predict.py
@@ -2,8 +2,6 @@
 from deeplog import DeepLog
 from deeplog.preprocessor import Preprocessor
 from sklearn.metrics import classification_report
-from deeplog_metrics import get_ind_metrics, is_anomaly, save_model, load_model
-
 
 import torch
 import aws_tools
@@ -27,104 +25,105 @@ import json
 from deep_log_metrics import get_ind_metrics, is_anomaly, save_model, load_model
 
 
+def model_predict(file_name):
 
-##############################################################################
-#                                 Load data                                  #
-##############################################################################
+    ##############################################################################
+    #                                 Load data                                  #
+    ##############################################################################
 
-# Create preprocessor for loading data
-#length - tamanho da janela a ser considerada
-preprocessor = Preprocessor(
-    length  = 10,           # Extract sequences of 20 items
-    timeout = float('inf'), # Do not include a maximum allowed time between events
-)
+    # Create preprocessor for loading data
+    #length - tamanho da janela a ser considerada
+    preprocessor = Preprocessor(
+        length  = 10,           # Extract sequences of 20 items
+        timeout = float('inf'), # Do not include a maximum allowed time between events
+    )
 
-# Dowload the file from S3
-prefix = "clean/"
-s3_path = "deep_log"
-aws_tools.get_to_s3(f'cluster_{file_name}', prefix)
-aws_tools.get_to_s3('deeplog_model_v1.pth', s3_path)
-
-
-
-# Load normal data from s3
-X, y, label, mapping = preprocessor.text(
-    path    = f'cluster_{file_name}',
-    verbose = True,
-    # nrows   = 10_000, # Uncomment/change this line to only load a limited number of rows
-)
+    # Dowload the file from S3
+    prefix = "clean/"
+    s3_path = "deep_log"
+    aws_tools.get_to_s3(f'cluster_{file_name}', prefix)
+    aws_tools.get_to_s3('deeplog_model_v1.pth', s3_path)
 
 
 
-
-
-##############################################################################
-#                                 Load state of model                        #
-##############################################################################
-
-
-# Create DeepLog object
-#output_size - número de chaves diferentes, geralmente output_size = length
-deeplog = DeepLog(
-    input_size  = 30, # Number of different events to expect
-    hidden_size = 64 , # Hidden dimension, we suggest 64
-    output_size = 30, # Number of different events to expect
-)
-
-
-load_model(deeplog, 'deeplog_model_v1.pth')
-
-
-
-##############################################################################
-#                                Predict logs                                #
-##############################################################################
+    # Load normal data from s3
+    X, y, label, mapping = preprocessor.text(
+        path    = f'cluster_{file_name}',
+        verbose = True,
+        # nrows   = 10_000, # Uncomment/change this line to only load a limited number of rows
+    )
 
 
 
 
-# Predict normal data using deeplog
-y_pred, confidence = deeplog.predict(
-    X = X,
-    k = 5, # Change this value to get the top k predictions (called 'g' in DeepLog paper, see Figure 6)
-)
+
+    ##############################################################################
+    #                                 Load state of model                        #
+    ##############################################################################
+
+
+    # Create DeepLog object
+    #output_size - número de chaves diferentes, geralmente output_size = length
+    deeplog = DeepLog(
+        input_size  = 30, # Number of different events to expect
+        hidden_size = 64 , # Hidden dimension, we suggest 64
+        output_size = 30, # Number of different events to expect
+    )
+
+
+    load_model(deeplog, 'deeplog_model_v1.pth')
+
+
+
+    ##############################################################################
+    #                                Predict logs                                #
+    ##############################################################################
 
 
 
 
-#Considerando apenas o primeiro elemento do vetor de predição, o modelo acertou o próximo evento?
-individual_pred = get_ind_metrics(y, y_pred)
-
-
-## Upload results to S3
-s3_path = "deep_log"
-
-
-
-# vector of anomalies
-anomalies_normal = is_anomaly(y,y_pred)
+    # Predict normal data using deeplog
+    y_pred, confidence = deeplog.predict(
+        X = X,
+        k = 5, # Change this value to get the top k predictions (called 'g' in DeepLog paper, see Figure 6)
+    )
 
 
 
 
-##############################################################################
-#                                Upload results of data batch to s3          #
-##############################################################################
+    #Considerando apenas o primeiro elemento do vetor de predição, o modelo acertou o próximo evento?
+    individual_pred = get_ind_metrics(y, y_pred)
 
 
-res_dic = {
-    'individual_pred' : individual_pred,
-    'predictions': y_pred.cpu().numpy().tolist(),
-    'confidence': confidence.cpu().numpy().tolist(),
-    'anomalies': anomalies_normal.numpy().tolist()
-}
+    ## Upload results to S3
+    s3_path = "deep_log"
 
 
 
-file_name = file_name[:-4]
-with open(f"predict_{file_name}.json", "w") as outfile:
-    json.dump(res_dic, outfile)
+    # vector of anomalies
+    anomalies_normal = is_anomaly(y,y_pred)
 
 
-aws_tools.upload_to_s3(f'predict_{file_name}', s3_path)
-os.remove(f'predict_{file_name}'), os.remove(f'values_{file_name}'), os.remove(file_name)
+
+
+    ##############################################################################
+    #                                Upload results of data batch to s3          #
+    ##############################################################################
+
+
+    res_dic = {
+        'individual_pred' : individual_pred,
+        'predictions': y_pred.cpu().numpy().tolist(),
+        'confidence': confidence.cpu().numpy().tolist(),
+        'anomalies': anomalies_normal.numpy().tolist()
+    }
+
+
+
+    file_name = file_name[:-4]
+    with open(f"predict_{file_name}.json", "w") as outfile:
+        json.dump(res_dic, outfile)
+
+
+    aws_tools.upload_to_s3(f'predict_{file_name}', s3_path)
+    os.remove(f'predict_{file_name}'), os.remove(f'values_{file_name}'), os.remove(file_name)
--- apps/deep-log/deep_log_train.py
+++ apps/deep-log/deep_log_train.py
@@ -24,55 +24,58 @@ from deep_log_metrics import get_ind_metrics, is_anomaly, save_model, load_model
 
 
 
+def train_model(file_name):
+
+    ##############################################################################
+    #                                 Load data                                  #
+    ##############################################################################
+
+    # Create preprocessor for loading data
+    #length - tamanho da janela a ser considerada
+    preprocessor = Preprocessor(
+        length  = 10,           # Extract sequences of 20 items
+        timeout = float('inf'), # Do not include a maximum allowed time between events
+    )
+
+    # Dowload the file from S3
+    prefix = "clean/"
+    #aws_tools.get_to_s3(f'cluster_{file_name}', prefix)
+
+    # Load normal data from s3
+    X, y, label, mapping = preprocessor.text(
+        path    = f'cluster_{file_name}',
+        verbose = True,
+        # nrows   = 10_000, # Uncomment/change this line to only load a limited number of rows
+    )
 
 
-##############################################################################
-#                                 Load data                                  #
-##############################################################################
 
-# Create preprocessor for loading data
-#length - tamanho da janela a ser considerada
-preprocessor = Preprocessor(
-    length  = 10,           # Extract sequences of 20 items
-    timeout = float('inf'), # Do not include a maximum allowed time between events
-)
+    ##############################################################################
+    #                                 Train deeplog                              #
+    ##############################################################################
+    # Create DeepLog object
+    #output_size - número de chaves diferentes, geralmente output_size = length
+    deeplog = DeepLog(
+        input_size  = 30, # Number of different events to expect
+        hidden_size = 64 , # Hidden dimension, we suggest 64
+        output_size = 30, # Number of different events to expect
+    )
 
-# Dowload the file from S3
-prefix = "clean/"
-#aws_tools.get_to_s3(f'cluster_{file_name}', prefix)
-
-# Load normal data from s3
-X, y, label, mapping = preprocessor.text(
-    path    = f'cluster_{file_name}',
-    verbose = True,
-    # nrows   = 10_000, # Uncomment/change this line to only load a limited number of rows
-)
-
+    # Train deeplog
+    deeplog.fit(
+        X          = X,
+        y          = y,
+        epochs     = 30 ,
+        batch_size = 128,
+        optimizer  = torch.optim.Adam,
+    )
 
 
-##############################################################################
-#                                 Train deeplog                              #
-##############################################################################
-# Create DeepLog object
-#output_size - número de chaves diferentes, geralmente output_size = length
-deeplog = DeepLog(
-    input_size  = 30, # Number of different events to expect
-    hidden_size = 64 , # Hidden dimension, we suggest 64
-    output_size = 30, # Number of different events to expect
-)
+    save_model(deeplog, 'model_v1.pth')
 
-# Train deeplog
-deeplog.fit(
-    X          = X,
-    y          = y,
-    epochs     = 30 ,
-    batch_size = 128,
-    optimizer  = torch.optim.Adam,
-)
+    s3_path = "deep_log"
+    aws_tools.upload_to_s3('deeplog_model_v1.pth', s3_path)
+    os.remove('deeplog_model_v1.pth')
 
 
-save_model(deeplog, 'model_v1.pth')
 
-s3_path = "deep_log"
-aws_tools.upload_to_s3('deeplog_model_v1.pth', s3_path)
-#os.remove('deeplog_model_v1.pth')
