2c83a54392eadfa2d22ccbdc1094b8cd3aa40152
Pedro Alves <alvespedro769@usp.br>
2023-06-18 19:51:33 -0300

    a

--- apps/deep-log/deep_log.py
+++ apps/deep-log/deep_log.py
@@ -56,16 +56,6 @@ prefix = 'clean/'
 s3_path = "predict"
 file_to_run = list_s3_files(prefix)
 
-i = 0
-v = 3
+train_model(file_name = 'whatever', first_train=True, version = 10)
 for file_name in file_to_run:
-    if i == 0:
-        train_model(file_name, first_train=True, version = v)
-        model_predict(file_name)
-        i +=1
-        v+=1
-    else:
-        train_model(file_name, first_train=False, version = v)
-        model_predict(file_name)
-        v+=1
-
+    model_predict(file_name)
