65641ea11a345a24fa063b4c24a365259a243954
Pedro Alves <alvespedro769@usp.br>
2023-06-19 07:21:09 -0300

    deletes cache file for deep_log_predict

--- apps/deep-log/deep_log_predict.py
+++ apps/deep-log/deep_log_predict.py
@@ -24,7 +24,7 @@ import json
 
 from deep_log_metrics import get_ind_metrics, is_anomaly, save_model, load_model
 
-
+os.remove('__pycache__/deep_log_predict.cpython-39')
 def model_predict(file_list, get_weights = False):
 
     ##############################################################################
@@ -45,30 +45,25 @@ def model_predict(file_list, get_weights = False):
     #descomentar depois
     #aws_tools.get_to_s3(f'cleansed_{file_name}', prefix)
     
+    
 
+    aws_tools.get_to_s3('deeplog_model_v10.pth', s3_path)
+    # Create DeepLog object
+    #output_size - número de chaves diferentes, geralmente output_size = length
+    deeplog = DeepLog(
+        input_size  = 30, # Number of different events to expect
+        hidden_size = 64 , # Hidden dimension, we suggest 64
+        output_size = 30, # Number of different events to expect
+    )
 
-
+    load_model(deeplog, 'deeplog_model_v10.pth')
 
     for file in file_list:
-
-        
-        aws_tools.get_to_s3('deeplog_model_v10.pth', s3_path)
-
 
         ##############################################################################
         #                                 Load state of model                        #
         ##############################################################################
-
-
-        # Create DeepLog object
-        #output_size - número de chaves diferentes, geralmente output_size = length
-        deeplog = DeepLog(
-            input_size  = 30, # Number of different events to expect
-            hidden_size = 64 , # Hidden dimension, we suggest 64
-            output_size = 30, # Number of different events to expect
-        )
-
-        load_model(deeplog, 'deeplog_model_v10.pth')
+        
 
         # Preprocessor its not implemented for .json files
         # in this chunk i convert the cluster entry in the .json to .txt 
@@ -156,7 +151,7 @@ def model_predict(file_list, get_weights = False):
         aws_tools.upload_to_s3(f'predict_{file_name}', s3_path)
         os.remove(f'predict_{file_name}')
         os.remove('tempfile_predict.txt')
-        os.remove('deeplog_model_v10.pth')
+        
         
     
     # with open(f"predict_{file_name}.json", "w") as outfile:
