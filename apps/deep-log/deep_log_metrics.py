90cd2c214dad3efedefd91ae42959d574bccd402
Pedro Alves <alvespedro769@usp.br>
2023-06-19 08:36:06 -0300

    deletes cache and adds encoder to read weights file

--- apps/deep-log/deep_log_metrics.py
+++ apps/deep-log/deep_log_metrics.py
@@ -16,8 +16,11 @@ def load_model(deeplog,path_to_pth):
     '''
     Reloads deeplog model
     '''
+    import pickle
+    pickle.load = partial(pickle.load, encoding="latin1")
+    pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
 
-    deeplog.load_state_dict(torch.load(path_to_pth))
+    deeplog.load_state_dict(torch.load(path_to_pth, map_location=lambda storage, loc: storage, pickle_module=pickle))
 
     return deeplog
 
