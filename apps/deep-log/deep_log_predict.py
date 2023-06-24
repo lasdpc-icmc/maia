7f67e306efa887ad8661d7c8d8f6a2fc07d1e9d3
Diego Frazatto Pedroso <diego.pedroso@vodafone.com>
2023-06-24 19:59:35 +0100

    remove duplicated uploaded function

--- apps/deep-log/deep_log_predict.py
+++ apps/deep-log/deep_log_predict.py
@@ -134,9 +134,4 @@ def model_predict(file_name):
 
 
     aws_tools.upload_to_s3(f'predict_{file_name}.json', s3_path)
-    os.remove(f'predict_{file_name}')
-    os.remove('tempfile_predict.txt')
-    
-
-    aws_tools.upload_to_s3(f'predict_{file_name}.json', s3_path)
     os.remove(f'predict_{file_name}.json')
