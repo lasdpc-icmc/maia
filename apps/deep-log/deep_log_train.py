520ffe682ff857fae55be8bdb8684bfa78169dfa
Diego Frazatto Pedroso <diego.pedroso@vodafone.com>
2023-10-01 19:40:36 +0100

    change code to use functions and delete unusual unusual code

--- apps/deep-log/deep_log_train.py
+++ apps/deep-log/deep_log_train.py
@@ -1,61 +1,37 @@
 
-import torch
-import aws_tools
-import os
-from drain_parser import file_name
-
-
-
-# Import DeepLog and Preprocessor
 from deeplog import DeepLog
 from deeplog.preprocessor import Preprocessor
 from sklearn.metrics import classification_report
-
-
-# Imports for showing metrics
+from data_cleaning import clean_sock, read_logs, write_logs
+from deep_log_metrics import get_ind_metrics, is_anomaly, save_model, load_model
+import torch
+import aws_tools
+import os
 import numpy as np
 import json
 
+MODEL_STABLE_VERSION = os.environ['MODEL_STABLE_VERSION']
 
 
-from data_cleaning import clean_sock, read_logs, write_logs
-from deep_log_metrics import get_ind_metrics, is_anomaly, save_model, load_model
+def train_model(file_name, first_train=False):
 
-
-
-
-def train_model(file_name, first_train = False, version = 2):
+    file_name = file_name[:-4]
 
     ##############################################################################
     #                                 Load data                                  #
     ##############################################################################
 
     # Create preprocessor for loading data
-    #length - tamanho da janela a ser considerada
+
     preprocessor = Preprocessor(
-        length  = 10,           # Extract sequences of 20 items
-        timeout = float('inf'), # Do not include a maximum allowed time between events
+        length=20,           # Extract sequences of 20 items
+        # Do not include a maximum allowed time between events
+        timeout=float('inf'),
     )
 
     # Dowload the file from S3
     prefix = "clean/"
     aws_tools.get_to_s3(f"cleansed_{file_name}.json", prefix)
-
-
-    # file_to_train = aws_tools.list_s3_files(prefix)
-    # for file_name in file_to_train:
-    #     aws_tools.get_to_s3(file_name, prefix)
-
-    #     file = open(file_name)
-    #     cleansed_file = json.load(file)
-
-    #     with open('tempfile_train.txt', 'a') as f:
-    #         for i in cleansed_file['cluster']:
-    #             f.write(str(i) + ' ')
-
-    #     os.remove(file_name)
-        
-
 
     file = open(f"cleansed_{file_name}.json")
     cleansed_file = json.load(file)
@@ -64,14 +40,11 @@ def train_model(file_name, first_train = False, version = 2):
         for i in cleansed_file['cluster']:
             f.write(str(i) + ' ')
 
-
     X, y, label, mapping = preprocessor.text(
-        path    = 'tempfile_train.txt',
-        verbose = True,
+        path='tempfile_train.txt',
+        verbose=True,
         # nrows   = 10_000, # Uncomment/change this line to only load a limited number of rows
     )
-
-
 
     ##############################################################################
     #                                 Train deeplog                              #
@@ -79,67 +52,50 @@ def train_model(file_name, first_train = False, version = 2):
     s3_path = "deeplog_statemodel"
     if first_train == True:
         # Create DeepLog object
-        #output_size - número de chaves diferentes, geralmente output_size = length
+        # output_size - número de chaves diferentes, geralmente output_size = length
         deeplog = DeepLog(
-            input_size  = 30, # Number of different events to expect
-            hidden_size = 64 , # Hidden dimension, we suggest 64
-            output_size = 30, # Number of different events to expect
+            input_size=30,  # Number of different events to expect
+            hidden_size=64,  # Hidden dimension, we suggest 64
+            output_size=30,  # Number of different events to expect
         )
 
         # Train deeplog
         deeplog.fit(
-            X          = X,
-            y          = y,
-            epochs     = 100 ,
-            batch_size = 128,
-            optimizer  = torch.optim.Adam,
+            X=X,
+            y=y,
+            epochs=100,
+            batch_size=128,
+            optimizer=torch.optim.Adam,
         )
 
+        save_model(deeplog, MODEL_STABLE_VERSION)
+        aws_tools.upload_to_s3(MODEL_STABLE_VERSION, s3_path)
+        os.remove(MODEL_STABLE_VERSION)
 
-        save_model(deeplog, f'model_v{version}')
-
-        
-        aws_tools.upload_to_s3(f'deeplog_model_v{version}.pth', s3_path)
-        os.remove(f'deeplog_model_v{version}.pth')
-    
     else:
-        
-        aws_tools.get_to_s3(f'deeplog_model_v{version-1}.pth', s3_path)
 
-
-
-
+        aws_tools.get_to_s3(MODEL_STABLE_VERSION, s3_path)
 
         # Create DeepLog object
-        #output_size - número de chaves diferentes, geralmente output_size = length
+        # output_size - número de chaves diferentes, geralmente output_size = length
         deeplog = DeepLog(
-            input_size  = 30, # Number of different events to expect
-            hidden_size = 64 , # Hidden dimension, we suggest 64
-            output_size = 30, # Number of different events to expect
+            input_size=30,  # Number of different events to expect
+            hidden_size=64,  # Hidden dimension, we suggest 64
+            output_size=30,  # Number of different events to expect
         )
 
-
-        load_model(deeplog, f'deeplog_model_v{version-1}.pth')
+        load_model(deeplog, MODEL_STABLE_VERSION)
 
         # Train deeplog
         deeplog.fit(
-            X          = X,
-            y          = y,
-            epochs     = 20 ,
-            batch_size = 128,
-            optimizer  = torch.optim.Adam,
+            X=X,
+            y=y,
+            epochs=20,
+            batch_size=128,
+            optimizer=torch.optim.Adam,
         )
 
-
-        save_model(deeplog, f'model_v{version}')
+        save_model(deeplog, MODEL_STABLE_VERSION)
 
         s3_path = "deeplog_statemodel"
-        aws_tools.upload_to_s3(f'deeplog_model_v{version}.pth', s3_path)
-        os.remove(f'deeplog_model_v{version}.pth')
-        os.remove(f'deeplog_model_v{version-1}.pth')
-    
-    os.remove('tempfile_train.txt')
-
-
-
-
+        aws_tools.upload_to_s3(MODEL_STABLE_VERSION, s3_path)
