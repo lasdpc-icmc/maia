f03b4c799312cdbdf5bd4ad750ef7ef1e4665498
lucasgpulcinelli <lucasegp@usp.br>
2023-12-01 16:07:06 -0300

    clean up deep_log.py and loki.py

--- apps/deep-log/deep_log.py
+++ apps/deep-log/deep_log.py
@@ -1,43 +1,26 @@
-import torch
 import aws_tools
 import os
-import json
-import boto3
-import subprocess
-import numpy as np
 import deep_log_train
 import deep_log_predict
 import drain_parser
 
-from deeplog import DeepLog
-from deeplog.preprocessor import Preprocessor
-from sklearn.metrics import classification_report
 from loki import get_loki_logs
 
-FIRST_TRANING = os.environ['FIRST_TRANING']
 LOKI_URL = os.environ['LOKI_URL']
 APP_NAME = os.environ['APP_NAME']
 TIME_RANGE = int(os.environ['TIME_RANGE'])
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_REGION = os.environ['AWS_REGION']
-S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 
 file_name = get_loki_logs(LOKI_URL, APP_NAME, TIME_RANGE,
-                          AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, S3_BUCKET_NAME)
+                          AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
 def main():
+    drain_parser.proccess_logs_files(file_name)
+    deep_log_train.train_model(file_name)
+    deep_log_predict.model_predict(file_name)
 
-    if FIRST_TRANING == 'True':
-        drain_parser.proccess_logs_files(file_name)
-        deep_log_train.train_model(file_name)
-        deep_log_predict.model_predict(file_name)
-        print(f"Training completed for {file_name}")
-    else:
-        drain_parser.proccess_logs_files(file_name)
-        deep_log_train.train_model(file_name)
-        deep_log_predict.model_predict(file_name)
-        print(f"Prediction completed for {file_name}")
+    print(f"Processing completed for {file_name}")
 
 
 if __name__ == "__main__":
--- apps/deep-log/loki.py
+++ apps/deep-log/loki.py
@@ -1,13 +1,10 @@
-import boto3
-import os
 import requests
 import json
 import time
-import aws_tools
 global file_name
 
 
-def get_loki_logs(LOKI_URL, APP_NAME, TIME_RANGE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, S3_BUCKET_NAME):
+def get_loki_logs(LOKI_URL, APP_NAME, TIME_RANGE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
     end_time = int(time.time())
     start_time = end_time - TIME_RANGE  # TIME_RANGE seconds ago
     query = '{namespace ="' + APP_NAME + '"}'
@@ -28,23 +25,23 @@ def get_loki_logs(LOKI_URL, APP_NAME, TIME_RANGE, AWS_ACCESS_KEY_ID, AWS_SECRET_
     response = requests.get(LOKI_URL + "/loki/api/v1/query_range", params=params,
                             headers=headers, auth=(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY))
 
-    if response.status_code == 200:
-        raw_logs = response.json()["data"]["result"]
-        file_name = f"{APP_NAME}_{int(time.time())}.txt"
-        with open(file_name, "w") as f:
-
-            for log in raw_logs:
-                stream = log["stream"]
-                log_entries = log["values"]
-                for entry in log_entries:
-                    timestamp = entry[0]
-                    message = entry[1]
-                    parsed_log = json.loads(message)
-                    f.write(f"{stream} {timestamp} {parsed_log}\n")
-
-        return file_name
-
-    else:
+    if response.status_code != 200:
         print("Error fetching logs from Loki API. Status code: ",
               response.status_code)
         print(response.content)
+        exit(-1)
+
+    raw_logs = response.json()["data"]["result"]
+    file_name = f"{APP_NAME}_{int(time.time())}.txt"
+    with open(file_name, "w") as f:
+
+        for log in raw_logs:
+            stream = log["stream"]
+            log_entries = log["values"]
+            for entry in log_entries:
+                timestamp = entry[0]
+                message = entry[1]
+                parsed_log = json.loads(message)
+                f.write(f"{stream} {timestamp} {parsed_log}\n")
+
+    return file_name
