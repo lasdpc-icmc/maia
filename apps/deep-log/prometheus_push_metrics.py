9f955a8814e09d5e2bc02543cdbaa6ebaec9df47
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-01-17 19:56:19 -0300

    change prom-exporter code to deep-log core

--- apps/deep-log/prometheus_push_metrics.py
+++ apps/deep-log/prometheus_push_metrics.py
@@ -1,76 +1,55 @@
 import prometheus_client
-import boto3
 import json
 import os
 
-def get_file_s3(file_name, prefix):
-    s3 = boto3.client('s3')
-    bucket_name = os.environ["S3_BUCKET_NAME"]
-    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
-    last_object = max(response['Contents'], key=lambda x: x['LastModified'])
-    last_object_key = last_object['Key']
-    s3.download_file(bucket_name, last_object_key, file_name)
+def push_metrics_prometheus(file_name):
+    registry = prometheus_client.CollectorRegistry()
 
+    precision = prometheus_client.Gauge(
+        "deep_log_precision", "the general precision for the predictions",
+        ["log", "type"], registry=registry)
 
-def get_json_s3(prefix):
-    get_file_s3("in", prefix)
+    accuracy = prometheus_client.Gauge(
+        "deep_log_accuracy", "general accuracy for deep_log", registry=registry)
 
-    f = open("in", "r")
-    jsonin = json.load(f)
-    f.close()
-    
-    return jsonin
+    buckets = [0.05] + [0.1*i for i in range(1, 10)] + [float("inf")]
 
-registry = prometheus_client.CollectorRegistry()
+    confidence = prometheus_client.Histogram(
+        "deep_log_confidence", "confidence for each prediction",
+        ["app", "log_prediced", "log"], buckets=buckets, registry=registry)
 
-precision = prometheus_client.Gauge(
-    "deep_log_precision", "the general precision for the predictions",
-    ["log", "type"], registry=registry)
+    anomalies = prometheus_client.Gauge(
+        "deep_log_anomalies", "number of anomalies for that batch", ["app", "log"],
+        registry=registry)
 
-accuracy = prometheus_client.Gauge(
-    "deep_log_accuracy", "general accuracy for deep_log", registry=registry)
+    # read the deep_log outputs from S3 as a json
+    jsonin = f"{file_name}_predict.json"
 
-buckets = [0.05] + [0.1*i for i in range(1, 10)] + [float("inf")]
+    # find the relation between log lines and the apps that generated them
+    apps_list = jsonin["app"]
 
-confidence = prometheus_client.Histogram(
-    "deep_log_confidence", "confidence for each prediction",
-    ["app", "log_prediced", "log"], buckets=buckets, registry=registry)
+    # parse the json and populate all metrics
+    s = "individual_pred"
+    for i, log in enumerate(jsonin[s]):
+        if log == "accuracy":
+            accuracy.set(jsonin[s][log])
+            continue
 
-anomalies = prometheus_client.Gauge(
-    "deep_log_anomalies", "number of anomalies for that batch", ["app", "log"],
-    registry=registry)
+        for precision_type in jsonin[s][log]:
+            precision.labels(log, precision_type).set(
+                jsonin[s][log][precision_type])
 
-# read the deep_log outputs from S3 as a json
-jsonin = get_json_s3("deep_log/")
+    for i, prediction in enumerate(jsonin["predictions"]):
+        confidence_point = jsonin["confidence"][i]
+        anomaly = jsonin["anomalies"][i]
 
-# find the relation between log lines and the apps that generated them
-apps_list = jsonin["app"]
+        for j, log_type in enumerate(prediction):
+            confidence.labels(apps_list[i].split('}')[0], prediction[0],
+                                 log_type).observe(confidence_point[j])
 
-# parse the json and populate all metrics
-s = "individual_pred"
-for i, log in enumerate(jsonin[s]):
-    if log == "accuracy":
-        accuracy.set(jsonin[s][log])
-        continue
+        anomalies.labels(apps_list[i].split('}')[0],
+                            prediction[0]).inc(1 if anomaly else 0)
 
-    for precision_type in jsonin[s][log]:
-        precision.labels(log, precision_type).set(
-            jsonin[s][log][precision_type])
-
-for i, prediction in enumerate(jsonin["predictions"]):
-    confidence_point = jsonin["confidence"][i]
-    anomaly = jsonin["anomalies"][i]
-
-    for j, log_type in enumerate(prediction):
-        confidence.labels(apps_list[i].split('}')[0], prediction[0],
-                             log_type).observe(confidence_point[j])
-
-    anomalies.labels(apps_list[i].split('}')[0],
-                        prediction[0]).inc(1 if anomaly else 0)
-
-
-# finally, use pushgateway to expose the metrics we just collected
-def push_metrics_prometheus () :
     prometheus_client.push_to_gateway(
-    os.environ["PUSHGATEWAY_URL"], "deep_log", registry)
+        os.environ["PUSHGATEWAY_URL"], "deep_log", registry)
     print("Successfully pushed metrics to Prometheus")
