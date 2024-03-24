95f6e185f20dc89053eaafc3cf9028bba674e83c
lucasgpulcinelli <lucasegp@usp.br>
2024-03-24 19:59:22 -0300

    Change analysis via logit model

--- apps/metric-analysis/metricanalysis.py
+++ apps/metric-analysis/metricanalysis.py
@@ -1,17 +1,30 @@
 import pandas as pd
+import statsmodels.api as sm
+from sklearn.preprocessing import StandardScaler
 
 
 def correlate(timestamps, metrics, test_seconds):
     failure_df = timestamps_to_df(timestamps, test_seconds)
 
-    correlations = {}
+    individual_metrics = pd.DataFrame()
+
     for name in metrics:
         metric = pd.DataFrame(metrics[name])
+        metric = metric.rename(columns=lambda subname: f'{name}-{subname}')
+        drop_constants(metric)
+        individual_metrics = pd.concat([individual_metrics, metric], axis=1)
 
-        corr = metric.corrwith(failure_df.value)
-        correlations[name] = dict(zip(metric.keys(), corr))
+    metrics_scaled = StandardScaler().fit_transform(individual_metrics)
 
-    return correlations
+    regression = sm.Logit(failure_df["value"],
+                          metrics_scaled).fit(method="bfgs")
+    return regression.conf_int(0.05).reindex(individual_metrics.columns)
+
+
+def drop_constants(df):
+    for column in df:
+        if df[column].std() <= 0.05:
+            del df[column]
 
 
 def timestamps_to_df(timestamps, test_seconds):
