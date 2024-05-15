06b6ee1ea7bcc15f0a57a762a6dd1e7cb56aaa20
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-05-15 17:00:48 +0100

    upgrade EKS cluster

--- infrastructure/terraform/certmanager.tf
+++ infrastructure/terraform/certmanager.tf
@@ -10,3 +10,18 @@ resource "helm_release" "cert_manager" {
   create_namespace = true
   depends_on       = [module.eks]
 }
+
+variable "manifests_path" {
+  default = "certs/prod"
+}
+
+data "local_file" "manifests" {
+  count    = length(fileset(var.manifests_path, "*.yaml"))
+  filename = "${var.manifests_path}/${fileset(var.manifests_path, "*.yaml")[count.index]}"
+}
+
+resource "kubectl_manifest" "apply_manifests" {
+  count      = length(data.local_file.manifests)
+  yaml_body  = data.local_file.manifests[count.index].content
+  depends_on = [helm_release.cert_manager]
+}
