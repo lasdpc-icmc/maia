b7e07723bba13b4a00c125648e93c70172941995
lucasgpulcinelli <lucasegp@usp.br>
2024-05-26 12:37:40 -0300

    add istio crds helm chart

--- infrastructure/terraform/istio.tf
+++ infrastructure/terraform/istio.tf
@@ -1,3 +1,17 @@
+resource "helm_release" "istio_crds" {
+  name              = "istio-crds"
+  chart             = "base"
+  repository        = "https://istio-release.storage.googleapis.com/charts"
+  version           = "1.22.0"
+  timeout           = "600"
+  dependency_update = true
+  values            = [templatefile("helm-manifests/istio-base.tpl", { environment = var.environment })]
+  namespace         = "istio-system"
+  create_namespace  = true
+  depends_on        = [module.eks]
+}
+
+
 resource "helm_release" "istio" {
   name              = "istio"
   chart             = "istiod"
@@ -8,6 +22,6 @@ resource "helm_release" "istio" {
   values            = [templatefile("helm-manifests/istio.tpl", { environment = var.environment })]
   namespace         = "istio-system"
   create_namespace  = true
-  depends_on        = [module.eks]
+  depends_on        = [module.eks, helm_release.istio_crds]
 }
 
