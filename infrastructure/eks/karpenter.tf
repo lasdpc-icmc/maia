05ed5190bb4ef9dfef00096eef88f013a625d6e7
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-06-16 21:15:38 +0100

    deploy karpenter

--- infrastructure/eks/karpenter.tf
+++ infrastructure/eks/karpenter.tf
@@ -20,12 +20,12 @@ data "aws_iam_policy_document" "karpenter_controller_assume_role_policy" {
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = [ "system:serviceaccount:karpenter:karpenter" ]
+      variable = "${module.eks.cluster_oidc_issuer_url}:sub"
+      values   = ["system:serviceaccount:karpenter:karpenter"]
     }
 
     principals {
-      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      identifiers = [module.eks.oidc_provider_arn]
       type        = "Federated"
     }
   }
@@ -93,19 +93,6 @@ resource "aws_iam_instance_profile" "karpenter" {
   name       = "KarpenterNodeInstanceProfile"
   role       = aws_iam_role.nodes.name
   depends_on = [aws_iam_role_policy_attachment.aws_load_balancer_controller_attach]
-}
-
-resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [
-    data.tls_certificate.eks.certificates[0].sha1_fingerprint,
-    "9e99a48a9960b14926bb7f3b02e22da2b0ab7280"
-  ]
-  url             = flatten(concat(aws_eks_cluster.eks_cluster[*].identity[*].oidc.0.issuer, [""]))[0]
-}
-
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
 ####################
