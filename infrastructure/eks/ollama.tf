6edc7f63d5d742581cb1815986ee24953d999f9c
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-06-17 21:51:54 +0100

    deploy ollama and web-ui FE prompt on EKS

--- infrastructure/eks/ollama.tf
+++ infrastructure/eks/ollama.tf
@@ -1,14 +1,15 @@
 resource "helm_release" "ollama" {
-  name              = "open-webui"
-  chart             = "open-webui"
-  repository        = "https://helm.openwebui.com"
-  version           = "3.0.4"
-  timeout           = "600"
-  dependency_update = true
-  values            = [templatefile("helm-manifests/ollama.tpl", { environment = var.environment })]
-  namespace         = "ollama"
-  create_namespace  = true
-  depends_on        = [helm_release.cert_manager]
+  name                       = "open-webui"
+  chart                      = "open-webui"
+  repository                 = "https://helm.openwebui.com/"
+  version                    = "3.0.4"
+  timeout                    = "600"
+  dependency_update          = true
+  values                     = [templatefile("helm-manifests/ollama.tpl", { environment = var.environment })]
+  namespace                  = "ollama"
+  create_namespace           = true
+  disable_openapi_validation = true
+  depends_on                 = [helm_release.cert_manager]
 }
 
 resource "kubernetes_manifest" "ollama_prod_certificate" {
