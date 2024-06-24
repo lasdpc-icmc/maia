0eccee2bd7dea7a45a40e72faafc80bd8c44b4c0
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-06-24 19:57:51 +0100

    deploy istio

--- infrastructure/eks/istio.tf
+++ infrastructure/eks/istio.tf
@@ -2,7 +2,7 @@ resource "helm_release" "istio_base" {
   name              = "istio-base"
   chart             = "base"
   repository        = "https://istio-release.storage.googleapis.com/charts"
-  version           = "1.22.0"
+  version           = "1.22.1"
   timeout           = "600"
   dependency_update = true
   values            = [templatefile("helm-manifests/istio-base.tpl", { environment = var.environment })]
@@ -16,7 +16,7 @@ resource "helm_release" "istio" {
   name              = "istio"
   chart             = "istiod"
   repository        = "https://istio-release.storage.googleapis.com/charts"
-  version           = "1.22.0"
+  version           = "1.22.1"
   timeout           = "600"
   dependency_update = true
   values            = [templatefile("helm-manifests/istiod.tpl", { environment = var.environment })]
@@ -28,7 +28,7 @@ resource "helm_release" "istio_ingress" {
   name              = "istio-ingress"
   chart             = "gateway"
   repository        = "https://istio-release.storage.googleapis.com/charts"
-  version           = "1.22.0"
+  version           = "1.22.1"
   timeout           = "600"
   dependency_update = true
   values            = [templatefile("helm-manifests/istio-ingress.tpl", { environment = var.environment })]
