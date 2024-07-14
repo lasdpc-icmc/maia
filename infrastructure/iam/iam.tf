5efa55b0feee6e9a6be7c1e4ad6836921cb8f244
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-07-14 19:40:23 +0100

    change Cloudwatch retention and change IAM ACL Role

--- infrastructure/iam/iam.tf
+++ infrastructure/iam/iam.tf
@@ -73,3 +73,45 @@ resource "aws_iam_policy_attachment" "tag_read_attachment" {
   policy_arn = aws_iam_policy.tag_read_policy.arn
   roles      = [aws_iam_role.grafana_read_billing_role.name]
 }
+
+resource "aws_iam_policy" "eks_admin_policy" {
+  name        = "eks-admin-policy"
+  description = "EKS admin policy for managing EKS clusters"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "eks_admin_role" {
+  name = "eks-admin-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      for user, role in local.users_and_groups.users : {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.this.account_id}:user/${user}"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_admin_policy_attachment" {
+  for_each = { for idx, policy_arn in local.policies : idx => policy_arn }
+
+  role       = aws_iam_role.eks_admin_role.name
+  policy_arn = each.value
+}
