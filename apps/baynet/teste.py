6a46ae0c9bfd6478a67a20b43119b792d4f94e75
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-07-15 23:27:38 +0100

    set baynet initial setup

--- apps/baynet/teste.py
+++ apps/baynet/teste.py
@@ -4,7 +4,6 @@ from pgmpy.factors.discrete import TabularCPD
 from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.inference import VariableElimination
 
-# Example data
 data = pd.DataFrame({
     'ServiceA_ResponseTime': [0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
     'ServiceB_ErrorRate': [1, 1, 0, 0, 0, 1, 1, 0, 0, 1],
@@ -12,15 +11,12 @@ data = pd.DataFrame({
     'Incident': [1, 1, 0, 0, 0, 1, 1, 0, 0, 1]
 })
 
-# Define the structure of the Bayesian Network
 model = BayesianNetwork([('ServiceA_ResponseTime', 'Incident'), 
                          ('ServiceB_ErrorRate', 'Incident'),
                          ('ServiceC_CPUUsage', 'Incident')])
 
-# Fit the model using MaximumLikelihoodEstimator
 model.fit(data, estimator=MaximumLikelihoodEstimator)
 
-# Perform inference
 infer = VariableElimination(model)
 result = infer.map_query(variables=['ServiceA_ResponseTime', 'ServiceB_ErrorRate', 'ServiceC_CPUUsage'], evidence={'Incident': 1})
 print(result)
