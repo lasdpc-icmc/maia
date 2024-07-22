5c5251090d57462db271166cbe5fcfaf173077a2
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-07-22 22:37:15 +0100

    get k:v from traces and store as csv file

--- apps/baynet/tempo_get_values.py
+++ apps/baynet/tempo_get_values.py
@@ -2,11 +2,12 @@ import requests
 from datetime import datetime, timedelta, timezone
 import json
 import os
+import csv
 
-BASE_URL = int(os.environ["BASE_URL"])
+BASE_URL = os.environ["BASE_URL"]
 TRACE_TIME_RANGE_MINUTES = int(os.environ["TRACE_TIME_RANGE_MINUTES"])
-SAVE_DIRECTORY = int(os.environ["SAVE_DIRECTORY"])
-SERVICE_NAME= int(os.environ["SERVICE_NAME"])
+SAVE_DIRECTORY = os.environ["SAVE_DIRECTORY"]
+SERVICE_NAME = os.environ["SERVICE_NAME"]
 
 def get_trace_ids_from_last_n_minutes_for_service(service_name, minutes):
     end_time = datetime.now(timezone.utc)
@@ -53,44 +54,68 @@ def extract_span_attributes(span):
     for attr in span.get("attributes", []):
         key = attr.get("key")
         value = attr.get("value", {}).get("stringValue")
-
-        if key in ["http.status_code", "http.method"]:
-            attributes[key] = value
+        attributes[key] = value
 
     return attributes
 
-def generate_span_json(trace_data):
+def generate_span_list(trace_data):
     span_info_list = []
 
     if trace_data:
-        for span_set in trace_data.get("spanSets", []):
-            for span in span_set.get("spans", []):
-                span_info = {
-                    "spanID": span.get("spanID", "Unknown Span ID"),
-                    "startTime": span.get("startTimeUnixNano", "Unknown Start Time"),
-                    "durationNanos": span.get("durationNanos", "Unknown Duration"),
-                    "attributes": extract_span_attributes(span)
-                }
-                span_info_list.append(span_info)
+        for batch in trace_data.get("batches", []):
+            for scopeSpan in batch.get("scopeSpans", []):
+                for span in scopeSpan.get("spans", []):
+                    span_info = {
+                        "spanID": span.get("spanId", "Unknown Span ID"),
+                        "startTime": span.get("startTimeUnixNano", "Unknown Start Time"),
+                        "endTime": span.get("endTimeUnixNano", "Unknown End Time"),
+                        "attributes": extract_span_attributes(span)
+                    }
+                    span_info["durationNanos"] = int(span_info["endTime"]) - int(span_info["startTime"])
+                    span_info_list.append(span_info)
 
     return span_info_list
 
-def save_trace_to_file(trace_id, trace_data):
+def save_traces_to_csv(trace_data_list):
     os.makedirs(SAVE_DIRECTORY, exist_ok=True)
 
-    file_path = os.path.join(SAVE_DIRECTORY, f"{trace_id}.json")
+    file_path = os.path.join(SAVE_DIRECTORY, "traces.csv")
 
-    with open(file_path, 'w') as file:
-        json.dump(trace_data, file, indent=2)
-    print(f"Saved trace data to {file_path}")
+    # Get all KV from traces
+    all_keys = set()
+    for span_info_list in trace_data_list:
+        for span in span_info_list:
+            all_keys.update(span["attributes"].keys())
+
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        header = ["spanID", "startTime", "durationNanos"] + list(all_keys)
+        writer.writerow(header)
+
+        for span_info_list in trace_data_list:
+            for span in span_info_list:
+                row = [
+                    span.get("spanID"),
+                    span.get("startTime"),
+                    span.get("durationNanos")
+                ]
+                for key in all_keys:
+                    row.append(span["attributes"].get(key))
+                writer.writerow(row)
+    print(f"Saved trace on {file_path}")
 
 if __name__ == "__main__":
     service_name = SERVICE_NAME
     trace_ids = get_trace_ids_from_last_n_minutes_for_service(service_name, TRACE_TIME_RANGE_MINUTES)
     print(f"Trace IDs from the last {TRACE_TIME_RANGE_MINUTES} minutes for service '{service_name}':")
     
+    all_span_info_list = []
     for trace_id in trace_ids:
         print(f"Fetching details for Trace ID: {trace_id}")
         trace_data = get_trace_details(trace_id)
         if trace_data:
-            save_trace_to_file(trace_id, trace_data)
+            span_info_list = generate_span_list(trace_data)
+            all_span_info_list.append(span_info_list)
+    
+    if all_span_info_list:
+        save_traces_to_csv(all_span_info_list)
