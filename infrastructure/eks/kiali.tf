d32a9a7f03f63017f24e3960aaaa0a701c3ac350
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-08-06 00:50:00 +0100

    expose kiali over maia endpoint

--- infrastructure/eks/kiali.tf
+++ infrastructure/eks/kiali.tf
@@ -5,7 +5,11 @@ resource "helm_release" "kiali" {
   version           = "1.84.0"
   timeout           = "600"
   dependency_update = true
-  values            = [templatefile("helm-manifests/kiali.tpl", { environment = var.environment })]
   namespace         = "istio-system"
   depends_on        = [module.eks, helm_release.istio_base]
+  values = [templatefile("helm-manifests/kiali.tpl", {
+    environment   = var.environment,
+    client_id     = var.kiali_google_client_id,
+    client_secret = var.kiali_google_client_secret
+  })]
 }
