3b39e04ae111fc8140ac4c4f4626c797dd876604
Diego Frazatto Pedroso <pedrosodiego@gmail.com>
2024-11-13 18:16:37 -0300

    feat: change nodegraph deployment

--- apps/nodegraph-generator/src/main.py
+++ apps/nodegraph-generator/src/main.py
@@ -1,41 +1,3 @@
-import flask
-import requests
-import waitress
-import os
-import pandas as pd
-import redis
-
-app = flask.Flask('nodegraph-generator')
-
-# Configure Redis connection
-redis_host = os.environ.get('REDIS_URL')
-redis_port = os.environ.get('REDIS_PORT', 6379)
-redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-
-def load_outage_data():
-    '''
-    Load outage data from Redis into a dictionary.
-    '''
-    outage_map = {}
-    keys = redis_client.keys('down_probability:*')
-    for key in keys:
-        service_name = key.split(':')[-1]  # Extract the service name from the key
-        down_probability = float(redis_client.get(key))
-        outage_map[service_name] = down_probability
-    return outage_map
-
-def getNodeID(node_names, app_name):
-    '''
-    getNodeID gets the index of an app in the node_names list. If the app
-    wasn't seen before it appends the new app into the list.
-    '''
-    try:
-        return node_names.index(app_name) + 1
-    except ValueError:
-        newid = len(node_names) + 1
-        node_names.append(app_name)
-        return newid
-
 def genGraph(raw_metrics, outage_data):
     '''
     genGraph generates a json-like dictionary in the specification requested by
@@ -74,78 +36,30 @@ def genGraph(raw_metrics, outage_data):
     for name, details in node_names.items():
         down_probability = outage_data.get(name, 0.0)
         color = "green"  # Default color
+        subtitle = "Health - 65%"  # Default subtitle
 
         if down_probability < 0.45:
             color = "green"
+            if down_probability < 0.1:
+                subtitle = "Potential Error - 10%"
+            elif down_probability < 0.4:
+                subtitle = "Warning - 40%"
+            else:
+                subtitle = "Health - 65%"
         elif 0.45 <= down_probability <= 0.6:
             color = "yellow"
+            subtitle = "Warning - 40%"
         elif down_probability > 0.6:
             color = "red"
+            subtitle = "Potential Error - 10%"
 
         # Include mainStat for node display text
         nodes.append({
             'id': details['id'],
             'title': name, 
-            'mainStat': f"{down_probability * 100:.2f}%",
+            'mainStat': subtitle,
             'down_probability': down_probability,
             'color': color
         })
 
     return {"nodes": nodes, "edges": edges}
-
-@app.route('/api/graph/fields')
-def graphFields():
-    '''
-    graphFields handles the graph description API endpoint for Node Graph API.
-    '''
-    return flask.send_file('fields.json')
-
-@app.route('/api/graph/data')
-def graphData():
-    '''
-    graphData handles the graph data endpoint for the Node Graph API. It makes
-    a query in a Prometheus database that has Istio metrics and generates the
-    graph of apps that connect to each other in the period of time specified.
-    '''
-    raw_query = flask.request.args.get('query')
-    if raw_query is None:
-        return flask.Response('Bad request', 400)
-    raw_query = raw_query.split(' ')
-    if len(raw_query) < 2:
-        return flask.Response('Bad request', 400)
-
-    namespace = raw_query[0]
-    interval = raw_query[1]
-    offset = None if len(raw_query) < 3 else raw_query[2]
-
-    query = f'''
-    sum by (source_workload, destination_workload) (increase(
-        istio_requests_total{{source_workload_namespace="{namespace}", destination_workload!="unknown"}}[{interval}]
-        {f"offset {offset}" if offset is not None else ""}
-    ))
-    '''
-
-    try:
-        data = requests.get(f'{os.environ["PROMETHEUS_URL"]}/api/v1/query',
-                            {'query': query}).json()
-
-        if data['status'] != 'success':
-            return flask.Response('Prometheus failed', 500)
-        if data['data']['resultType'] != 'vector':
-            return flask.Response('Expected vector as query response', 500)
-
-        # Load outage data from Redis before generating the graph
-        outage_data = load_outage_data()  # No need to pass a file path now
-        return flask.jsonify(genGraph(data['data']['result'], outage_data))
-
-    except Exception as e:
-        return flask.Response(f'{type(e).__name__}: {e}', 500)
-
-@app.route('/api/health')
-def checkHealth():
-    '''
-    checkHealth handles the health-checking endpoint for the Node Graph API.
-    '''
-    return 'Healthy'
-
-waitress.serve(app, host='0.0.0.0', port=8080)
